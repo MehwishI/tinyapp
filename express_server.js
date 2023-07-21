fe296935f270ee65ac52e965e41eaf3480b81aa2
Mehwish Iqbal <mehwishned21@gmail.com>
2023-07-21 14:54:30 -0500

    Add /hello url and send response

--- express_server.js
+++ express_server.js
@@ -10,6 +10,9 @@ const urlDatabase = {
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
