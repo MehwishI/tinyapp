127fab28b92768ecbf8d2d420cd322ef0d24651a
Mehwish Iqbal <mehwishned21@gmail.com>
2023-07-22 14:33:14 -0500

    Add generateRandomstring function,

--- express_server.js
+++ express_server.js
@@ -4,10 +4,16 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  return Math.random().toString(36).slice(2);
+}
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+//for POST request, to parse the data sent as buffer into more readable data type
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -20,6 +26,9 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
@@ -31,6 +40,10 @@ app.get("/urls/:id", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("OK");
 });
 
 app.listen(PORT, () => {
