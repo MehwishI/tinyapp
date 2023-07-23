c9471b7efce059a97e887cf233f0cb078232987b
Mehwish Iqbal <mehwishned21@gmail.com>
2023-07-22 22:18:25 -0500

    Parse cookie and display in header

--- express_server.js
+++ express_server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
+var cookieParser = require("cookie-parser");
+
 const app = express();
 const PORT = 8080; // default port 8080
+app.use(morgan("dev"));
+
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 
@@ -24,17 +30,25 @@ app.get("/hello", (req, res) => {
 
 //renders view to show list of all urls
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  console.log(req.cookies["username"]);
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"] ? req.cookies["username"] : "",
+  };
   res.render("urls_index", templateVars);
 });
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"] ? req.cookies["username"] : "",
+  };
+  res.render("urls_new", templateVars);
 });
 //renders a view to show url details
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"],
   };
   res.render("urls_show", templateVars);
 });
