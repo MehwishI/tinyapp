97ec1b41c4863e79203c63cc3d814090b027b01a
Mehwish Iqbal <mehwishned21@gmail.com>
2023-07-23 00:47:58 -0500

    Add logout action and clear cookie

--- express_server.js
+++ express_server.js
@@ -94,6 +94,10 @@ app.post("/login", (req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("/urls");
 });
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Tinyapp server listening on port ${PORT}!`);
