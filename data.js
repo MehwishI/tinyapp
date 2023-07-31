65e81126c261120eb1491558f5050cac9eeadb09
Mehwish Iqbal <mehwishned21@gmail.com>
2023-07-31 11:28:35 -0500

    Implementing feedback- code decomposition

---
+++ data.js
@@ -0,0 +1,30 @@
+//database of urls
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+    createdDate: "",
+    url_visited: 0,
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+    createdDate: "",
+    url_visited: 0,
+  },
+};
+//users database
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+module.exports = { users, urlDatabase };
